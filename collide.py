624559615a44c5417dbd5a47e4867158e3b250c0
unknown <rodent05@gmail.com>
2015-04-18 13:13:57 -0500

    added thing

--- collide.py
+++ collide.py
@@ -1,1 +1,24 @@
 from data_classes import *
+
+
+
+
+def resolveCollision(a, b):
+    #relative velocity
+    rv = b.velocity - a.velocity
+
+    #relative velocity along normal
+    velAlongNormal = rv * normal
+
+    #do not resolve if they are heading away from eachother
+    if (velAlongNormal > 0):
+        return
+    e = min(a.restitution, b.restitution)
+
+    #impulse scalar
+    j = -(1.0+e) * velAlongNormal
+    j /= 1.0 / a.mass + 1.0 / b.mass
+
+    impulse = Point(j * normal.x, j * normal.y)
+    a.velocity -= impulse / a.mass
+    b.velocity += impulse / b.mass
